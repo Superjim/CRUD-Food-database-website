42b17238673cd011cd9b0b005ca3993976b6f0a6
Jim Jenkinson <jamesjenkinson10@gmail.com>
2022-05-11 14:57:32 +0100

    installed axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,75 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [foodName, setFoodName] = useState("");
+  const [foodWeight, setFoodWeight] = useState("");
+  const [kcal, setKcal] = useState("");
+  const [carbs, setCarbs] = useState("");
+  const [fat, setFat] = useState("");
+  const [protein, setProtein] = useState("");
+
+  const addFood = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      foodName: foodName,
+      foodWeight: foodWeight,
+      kcal: kcal,
+      carbs: carbs,
+      fat: fat,
+      protein: protein,
+    }).then(() => {
+      alert("Food Added");
+    });
+  };
+
   return (
     <div className="App">
       <h1>Calorie Tracker</h1>
       <div className="addFood">
         <label>Food Name:</label>
-        <input type="text" name="foodName" />
+        <input
+          type="text"
+          name="foodName"
+          onChange={(e) => setFoodName(e.target.value)}
+        />
+
         <label>Food Weight (grams):</label>
-        <input type="number" name="foodWeight" />
+        <input
+          type="number"
+          name="foodWeight"
+          onChange={(e) => setFoodWeight(e.target.value)}
+        />
+
         <label>kcal:</label>
-        <input type="number" name="kcal" />
+        <input
+          type="number"
+          name="kcal"
+          onChange={(e) => setKcal(e.target.value)}
+        />
+
         <label>Carbs (grams):</label>
-        <input type="number" name="carbs" />
+        <input
+          type="number"
+          name="carbs"
+          onChange={(e) => setCarbs(e.target.value)}
+        />
+
         <label>Fat (grams):</label>
-        <input type="number" name="fat" />
+        <input
+          type="number"
+          name="fat"
+          onChange={(e) => setFat(e.target.value)}
+        />
+
         <label>Protein (grams):</label>
-        <input type="number" name="protein" />
+        <input
+          type="number"
+          name="protein"
+          onChange={(e) => setProtein(e.target.value)}
+        />
+
+        <button onClick={addFood}>Add Food</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
@@ -11,6 +13,23 @@ const db = mysql.createPool({
 
 app.listen(3001, () => {
   console.log("server running port 3001");
+});
+
+app.use(bodyParser.urlencoded{extended: true}); 
+app.use(cors());
+
+app.post("/api/insert", (req, res) => {
+
+    const foodName = req.body.foodName;
+    const foodWeight = req.body.foodWeight;
+    const kcal = req.body.kcal;
+    const carbs = req.body.carbs;
+    const fat = req.body.fat;
+    const protein = req.body.protein;
+
+  const input =
+    "INSERT INTO foods (foodname, weight, kcal, carbs, fat, protein) VALUES (?,?,?,?,?,?)";
+  db.query(input, [foodName, foodWeight, kcal, carbs, fat, protein], (err, result) => {});
 });
 
 app.get("/", (req, res) => {
