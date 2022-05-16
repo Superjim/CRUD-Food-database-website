6293a19478c2b3b8138e4324642311f394331fc4
Jim Jenkinson <jamesjenkinson10@gmail.com>
2022-05-16 18:44:43 +0100

    fixed my previous "fix"

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
     Axios.get(
       "https://nutritional-information-mysql.herokuapp.com/api/get"
     ).then((response) => {
-      setFoodList(JSON.parse(response.data));
+      setFoodList(response.data);
     });
   });
 
